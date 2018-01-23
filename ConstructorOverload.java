0a6fecd8a09d92e42a532666d22c439c57f7940a
himanshuthanna <himanshuthanna10@gmail.com>
2018-01-23 20:50:39 +0400

    Add files via upload
    Constructor overloading example

---
+++ ConstructorOverload.java
@@ -0,0 +1,18 @@
+class K{
+	public static void main(String args[])
+	{ L a1=new L(3);
+	  System.out.println("hello");
+	  L a2=new L();
+	  System.out.println("hiii");
+	}
+	  
+}
+class L{
+	 L(int a)
+	 { System.out.println(a);
+	 }
+	 L()
+	   {
+		 System.out.println("lalu pagal hai");
+	   }
+}
