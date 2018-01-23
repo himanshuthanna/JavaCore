1a9093305971bfe886959d8c8f1eb6955d712e36
himanshuthanna <himanshuthanna10@gmail.com>
2018-01-23 20:47:32 +0400

    Add files via upload
    Constructor example

---
+++ constructor.java
@@ -0,0 +1,14 @@
+class A{
+	public static void main(String args[])
+	{ B a1=new B(3);
+	  System.out.println("hello");
+	  B a2=new B(12);
+	  System.out.println("hiii");
+	}
+	  
+}
+class B{
+	 B(int a)
+	 { System.out.println(a);
+	 }
+}
