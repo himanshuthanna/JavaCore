605e7541110c3603e43e988adcc40d3bfe3e9018
himanshuthanna <himanshuthanna10@gmail.com>
2018-01-26 13:41:59 +0400

    Add files via upload
    Inheritence with constructor concept

---
+++ InheritenceConstructor.java
@@ -0,0 +1,32 @@
+class A1{
+	  A1()
+	  { System.out.println("constr A1");
+	  }
+	 int x=0;
+	 void show()
+	     { System.out.println("heelo A");
+	     }   
+       }
+	   
+	 class B1 extends A1{
+	 int y=1;
+	 void print()
+	     { System.out.println("hello B");
+		   System.out.println(y);
+	     } 
+		 B1()
+	       { System.out.println("constr B1");
+	       }
+       		 
+       }
+	  class C1{
+		   public static void main(String args[])
+		   { B1 a1=new B1();
+		   a1.show();
+		   a1.print();
+		   
+		   }
+	  }
+		   
+	   
+	 
