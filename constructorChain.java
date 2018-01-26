ccf2fab09467f0d3715b0915707b390fa9bbfb88
himanshuthanna <himanshuthanna10@gmail.com>
2018-01-26 13:38:23 +0400

    Add files via upload
    Constructor chaining program

---
+++ constructorChain.java
@@ -0,0 +1,15 @@
+ class J{
+   public static void main(String args[])
+   { M a1=new M();
+   }
+ }
+class M{
+   M(int a)
+   { 
+    System.out.println("Hiii");
+   }
+    M()
+	   {this(4);
+		   System.out.println("helloo");
+	   }
+}   
